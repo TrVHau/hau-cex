a5248d6825f53d8ae6101997f520d7d0f609fdb3
TrVHau <haulg3@gmail.com>
2026-07-14 07:06:15 +0700

    fix: remove hardcode.

--- apps/backend/src/common/decorators/roles.decorator.ts
+++ apps/backend/src/common/decorators/roles.decorator.ts
@@ -1,3 +1,5 @@
+import { UserRole } from '../../generated/prisma';
 import { SetMetadata } from '@nestjs/common';
+import { ROLES_KEY } from '../constants/auth.constants';
 
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
+export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
